5dda43e11e5e3978a116254e6a52744bb61d3d4f
FloatinggOnion <jesseosems123@gmail.com>
2024-02-09 19:32:51 +0100

    Fix: static files settings

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,4 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Graphene
 GRAPHENE = {
     "SCHEMA": "hospital_management.schema.schema"
+}
+
+# Storages
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
